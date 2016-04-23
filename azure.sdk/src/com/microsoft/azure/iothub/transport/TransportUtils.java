38cc37043dec0d4ca2ba01c3da1eb5e7c2511570
Christos Sofronis <sofronis@gmail.com>
2016-04-23 14:21:47 +0200

    Azure SDK and examples init commit

---
+++ azure.sdk/src/com/microsoft/azure/iothub/transport/TransportUtils.java
@@ -0,0 +1,10 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+package com.microsoft.azure.iothub.transport;
+
+public class TransportUtils
+{
+    public static String javaDeviceClientIdentifier = "com.microsoft.azure.iothub-java-client/";
+    public static String clientVersion = "1.0.4";
+}
